347cbc3773c424211c9951ec48b3282bcc343baa
Marcos CARDINOT <mcardinot@gmail.com>
2016-10-11 20:11:11 +0100

    utils - add functions to convert list to matrix

--- src/util/util.h
+++ src/util/util.h
@@ -6,13 +6,25 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#include <QVector>
+
 namespace Util {
 
-    //! Fisher–Yates shuffle algorithm
-    //! https://bost.ocks.org/mike/shuffle
-    //! http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+    // Fisher–Yates shuffle algorithm
+    // https://bost.ocks.org/mike/shuffle
+    // http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
     QVector<int> shuffle(QVector<int> list);
 
+    // convert a linear index to row column
+    void ind2sub(const int ind, const int cols, int &row, int &col) {
+        row = ind / cols;
+        col = ind % cols;
+    }
+
+    // return a linear index
+    int getLinearIdx(const int row, const int col, const int cols) {
+        return row * cols + col;
+    }
 }
 
 #endif // UTIL_H
