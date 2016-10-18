52d947d91bf766eb9fc679dd30ace3133ed81089
Marcos CARDINOT <mcardinot@gmail.com>
2016-10-18 11:07:07 +0100

    WidgetProject - implementing slots (wip)

--- src/gui/widgetproject.cpp
+++ src/gui/widgetproject.cpp
@@ -1,7 +1,5 @@
-#include <QComboBox>
-#include <QDoubleSpinBox>
-#include <QLineEdit>
-#include <QSpinBox>
+#include <QMapIterator>
+#include <QtDebug>
 
 #include "widgetproject.h"
 #include "ui_widgetproject.h"
@@ -13,6 +11,12 @@ WidgetProject::WidgetProject(Project* project, QWidget *parent)
 {
     m_ui->setupUi(this);
 
+    connect(m_ui->bAdd, SIGNAL(clicked(bool)), this, SLOT(slotAdd()));
+    connect(m_ui->bAddRun, SIGNAL(clicked(bool)), this, SLOT(slotAddRun()));
+    connect(m_ui->bImport, SIGNAL(clicked(bool)), this, SLOT(slotImport()));
+    connect(m_ui->bRunAll, SIGNAL(clicked(bool)), this, SLOT(slotRunAll()));
+    connect(m_ui->bRunSelected, SIGNAL(clicked(bool)), this, SLOT(slotRunSelected()));
+
     //
     // simulation settings
     //
@@ -21,11 +25,13 @@ WidgetProject::WidgetProject(Project* project, QWidget *parent)
 
     QTreeWidgetItem* item = new QTreeWidgetItem(general);
     item->setText(0, "Seed");
-    m_ui->treeSettings->setItemWidget(item, 1, new QSpinBox());
+    m_treeSeed = new QSpinBox();
+    m_ui->treeSettings->setItemWidget(item, 1, m_treeSeed);
 
     item =new QTreeWidgetItem(general);
     item->setText(0, "Stop at");
-    m_ui->treeSettings->setItemWidget(item, 1, new QSpinBox());
+    m_treeStopAt = new QSpinBox();
+    m_ui->treeSettings->setItemWidget(item, 1, m_treeStopAt);
 
     //
     // population settings
@@ -35,25 +41,30 @@ WidgetProject::WidgetProject(Project* project, QWidget *parent)
 
     item =new QTreeWidgetItem(env);
     item->setText(0, "Type");
-    QComboBox* cb = new QComboBox();
-    cb->insertItem(Graph::SQUARE_GRID, "Square Grid (k=4)");
-    cb->insertItem(Graph::MOORE_GRID, "Moore Grid (k=8)");
-    m_ui->treeSettings->setItemWidget(item, 1, cb);
+    m_treeGraphType = new QComboBox();
+    m_treeGraphType->insertItem(Graph::SQUARE_GRID, "Square Grid (k=4)");
+    m_treeGraphType->insertItem(Graph::MOORE_GRID, "Moore Grid (k=8)");
+    m_ui->treeSettings->setItemWidget(item, 1, m_treeGraphType);
 
     item =new QTreeWidgetItem(env);
     item->setText(0, "Initial Population");
-    m_ui->treeSettings->setItemWidget(item, 1, new QLineEdit(m_project->getDir()));
+    m_treeInitialPop = new QLineEdit(m_project->getDir());
+    m_ui->treeSettings->setItemWidget(item, 1, m_treeInitialPop);
 
     //
     // model parameters
     //
     QTreeWidgetItem* params = new QTreeWidgetItem(m_ui->treeSettings);
     params->setText(0, "Model");
-    foreach (QMetaProperty mp, project->getModel()->params) {
+    QMapIterator<QString, QVariant> i(project->getModel()->params);
+    while (i.hasNext()) {
+        i.next();
         item = new QTreeWidgetItem(params);
-        item->setText(0, mp.name());
+        item->setText(0, i.key());
         QDoubleSpinBox* box = new QDoubleSpinBox();
         box->setDecimals(6);
+        box->setValue(i.value().toDouble());
+        m_treeModelParams.insert(i.key(), box);
         m_ui->treeSettings->setItemWidget(item, 1, box);
     }
 }
@@ -64,4 +75,37 @@ WidgetProject::~WidgetProject()
     m_ui = NULL;
 }
 
+void WidgetProject::slotAdd()
+{
+    Graph::GraphType gtype = (Graph::GraphType) m_treeGraphType->currentIndex();
+    Graph* g = new Graph(m_treeInitialPop->text(), gtype);
 
+    QVariantMap params;
+    QMapIterator<QString, QDoubleSpinBox*> i(m_treeModelParams);
+    while (i.hasNext()) {
+        i.next();
+        params.insert(i.key(), i.value()->value());
+    }
+
+    m_project->newExperiment(g, params);
+}
+
+void WidgetProject::slotAddRun()
+{
+
+}
+
+void WidgetProject::slotImport()
+{
+
+}
+
+void WidgetProject::slotRunAll()
+{
+
+}
+
+void WidgetProject::slotRunSelected()
+{
+
+}
--- src/gui/widgetproject.h
+++ src/gui/widgetproject.h
@@ -1,6 +1,11 @@
 #ifndef WIDGETPROJECT_H
 #define WIDGETPROJECT_H
 
+#include <QComboBox>
+#include <QDoubleSpinBox>
+#include <QLineEdit>
+#include <QMap>
+#include <QSpinBox>
 #include <QWidget>
 
 #include "core/project.h"
@@ -17,9 +22,22 @@ public:
     explicit WidgetProject(Project* project, QWidget *parent = 0);
     ~WidgetProject();
 
+private slots:
+    void slotAdd();
+    void slotAddRun();
+    void slotImport();
+    void slotRunAll();
+    void slotRunSelected();
+
 private:
     Ui::WidgetProject* m_ui;
     Project* m_project;
+
+    QComboBox* m_treeGraphType;
+    QLineEdit* m_treeInitialPop;
+    QSpinBox* m_treeSeed;
+    QSpinBox* m_treeStopAt;
+    QMap<QString, QDoubleSpinBox*> m_treeModelParams;
 };
 
 #endif // WIDGETPROJECT_H
