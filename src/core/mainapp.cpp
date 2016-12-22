fbb98483d69884cf06948dc41a48348761a2b8b3
Marcos CARDINOT <mcardinot@gmail.com>
2016-12-22 02:10:46 +0000

    MainApp (WIP)

--- src/core/mainapp.cpp
+++ src/core/mainapp.cpp
@@ -3,61 +3,117 @@
  * @author Marcos Cardinot <mcardinot@gmail.com>
  */
 
+#include <QJsonObject>
 #include <QPluginLoader>
 #include <QtDebug>
 
-#include "core/graph.h"
 #include "core/mainapp.h"
 #include "core/project.h"
+#include "utils/utils.h"
 
-MainApp::MainApp()
-    : m_processesMgr(new ProcessesMgr())
+MainApp::MainApp(): m_processesMgr(new ProcessesMgr())
 {
-    m_generalProperties << GENERAL_PROPERTY_NAME_AGENTS
-                        << GENERAL_PROPERTY_NAME_GRAPHTYPE
-                        << GENERAL_PROPERTY_NAME_SEED
-                        << GENERAL_PROPERTY_NAME_STOPAT;
+    m_generalProperties << GENERAL_PARAMETER_AGENTS
+                        << GENERAL_PARAMETER_GRAPHID
+                        << GENERAL_PARAMETER_SEED
+                        << GENERAL_PARAMETER_STOPAT;
+
+    //loadModelPlugin("/home/cardinot/dev/evoplex/evoplex/build2/models/libevoplex_models.so");
 }
 
 MainApp::~MainApp()
 {
-    qDeleteAll(m_models);
     qDeleteAll(m_projects);
+    qDeleteAll(m_models);
+    qDeleteAll(m_graphs);
     delete m_processesMgr;
     m_processesMgr = NULL;
 }
 
-int MainApp::loadModel(QString path)
+const QString& MainApp::loadGraphPlugin(QString path)
 {
     QPluginLoader loader(path);
+    QJsonObject json = loader.metaData();
     QObject* obj = loader.instance();
-    if (!obj) {
-        qWarning() << "[MainApp] unable to load the model" << path;
-        return -1;
+
+    if (json.isEmpty() || !obj) {
+        qWarning() << "[MainApp] unable to load the graph" << path;
+        return NULL;
+    } else if (!json.contains("uid")) {
+        qWarning() << "[MainApp] unable to load the graph."
+                   << "'uid' cannot be empty" << path;
+        delete obj;
+        return NULL;
+    } else if (m_models.contains(json["uid"].toString())) {
+        qWarning() << "[MainApp] unable to load the graph."
+                   << "'uid' must be unique!" << path;
+        delete obj;
+        return NULL;
     }
 
-    Model* model = new Model();
-    model->factory = qobject_cast<IModelFactory*>(obj);
-    model->name = model->factory->name();
-    model->description = model->factory->description();
-    model->author = model->factory->author();
-    if (model->name.isEmpty()) {
-        qWarning() << "[MainApp] unable to load the model. Name cannot be empty" << path;
-        return -1;
-    }
+    GraphPlugin* graph = new GraphPlugin();
+    graph->uid = json["uid"].toString();
+    graph->author = json["author"].toString();
+    graph->name = json["name"].toString();
+    graph->description = json["description"].toString();
+    graph->graphParamsSpace = json["graphParamsSpace"].toObject().toVariantHash();
+    graph->factory = qobject_cast<IPluginGraph*>(obj);
+    m_graphs.insert(graph->uid, graph);
 
-    IModel* mi = model->factory->create();
-    model->defaultModelParams = mi->getModelParams();
-    model->defaultInspectorParams = mi->getInspectorParams();
-    model->agentParamsDomain = mi->agentParamsDomain();
-    delete mi;
-
-    int modelId = m_models.size();
-    m_models.insert(modelId, model);
-    return modelId;
+    return graph->uid;
 }
 
-int MainApp::newProject(int modelId, const QString& name,
+const QString& MainApp::loadModelPlugin(QString path)
+{
+    QPluginLoader loader(path);
+    QJsonObject json = loader.metaData();
+    QObject* obj = loader.instance();
+
+    if (json.isEmpty() || !obj) {
+        qWarning() << "[MainApp] unable to load the model" << path;
+        return NULL;
+    } else if (!json.contains("uid")) {
+        qWarning() << "[MainApp] unable to load the model."
+                   << "'uid' cannot be empty" << path;
+        delete obj;
+        return NULL;
+    } else if (m_models.contains(json["uid"].toString())) {
+        qWarning() << "[MainApp] unable to load the graph."
+                   << "'uid' must be unique!" << path;
+        delete obj;
+        return NULL;
+    }
+
+    // lets deny the properties which might be used by the GUI
+    QVariantHash agentParamsSpace = json["agentParamsSpace"].toObject().toVariantHash();
+    QStringList reservedParams;
+    reservedParams << "id" << "x" << "y" << "z";
+    foreach (QString r, reservedParams) {
+        if (agentParamsSpace.contains(r)) {
+            qWarning() << "[MainApp] unable to load the model."
+                       << "The agent properties 'id', 'x', 'y' and 'z' are reserved and must not be used in the model."
+                       << path;
+            delete obj;
+            return NULL;
+        }
+    }
+
+    ModelPlugin* model = new ModelPlugin();
+    model->uid = json["uid"].toString();
+    model->author = json["author"].toString();
+    model->name = json["name"].toString();
+    model->description = json["description"].toString();
+    model->agentParamsSpace = agentParamsSpace;
+    model->modelParamsSpace = json["modelParamsSpace"].toObject().toVariantHash();
+    model->defaultAgentParams = Utils::minParams(model->agentParamsSpace);
+    model->defaultModelParams = Utils::minParams(model->modelParamsSpace);
+    model->factory = qobject_cast<IPluginModel*>(obj);
+    m_models.insert(model->uid, model);
+
+    return model->uid;
+}
+
+int MainApp::newProject(const QString& modelId, const QString& name,
                         const QString& descr, const QString& dir)
 {
     int projId = m_projects.size();
--- src/core/mainapp.h
+++ src/core/mainapp.h
@@ -7,57 +7,73 @@
 #define MAINAPP_H
 
 #include <QList>
-#include <QMetaProperty>
-#include <QObject>
 #include <QVariantHash>
 
-#include "core/interfaces.h"
+#include "core/abstractgraph.h"
+#include "core/abstractmodel.h"
 #include "core/processesmgr.h"
 
 // these constants hold the name of the properties used in any simulation
-#define GENERAL_PROPERTY_NAME_AGENTS "agents"
-#define GENERAL_PROPERTY_NAME_GRAPHTYPE "graphType"
-#define GENERAL_PROPERTY_NAME_SEED "seed"
-#define GENERAL_PROPERTY_NAME_STOPAT "stopAt"
+#define GENERAL_PARAMETER_AGENTS "agents"
+#define GENERAL_PARAMETER_GRAPHID "graphId"
+#define GENERAL_PARAMETER_SEED "seed"
+#define GENERAL_PARAMETER_STOPAT "stopAt"
 
 class Project;
 class Simulation;
 
-class MainApp: public QObject
+class MainApp
 {
-    Q_OBJECT
-
 public:
-    struct Model {
+    struct GraphPlugin {
+        QString uid;
         QString author;
         QString name;
         QString description;
-        QVariantHash agentParamsDomain;
+        QVariantHash graphParamsSpace;
+        IPluginGraph* factory;
+    };
+
+    struct ModelPlugin {
+        QString uid;
+        QString author;
+        QString name;
+        QString description;
+        QVariantHash agentParamsSpace;
+        QVariantHash modelParamsSpace;
+        QVariantHash defaultAgentParams;
         QVariantHash defaultModelParams;
-        QVariantHash defaultInspectorParams;
-        IModelFactory* factory;
+        IPluginModel* factory;
     };
 
     MainApp();
     ~MainApp();
 
-    // load model from a .so file
-    // return the model id
-    int loadModel(QString path);
+    // load graph from a .so file
+    // return the graph uid
+    const QString &loadGraphPlugin(QString path);
 
-    int newProject(int modelId, const QString& name,
+    // load model from a .so file
+    // return the model uid
+    const QString &loadModelPlugin(QString path);
+
+    int newProject(const QString &modelId, const QString& name,
             const QString& descr, const QString& dir);
 
     inline ProcessesMgr* getProcessesMgr() { return m_processesMgr; }
-    inline const QHash<int, Model*>& getModels() { return m_models; }
+    inline const QHash<QString, GraphPlugin*>& getGraphs() { return m_graphs; }
+    inline const QHash<QString, ModelPlugin*>& getModels() { return m_models; }
     inline const QHash<int, Project*>& getProjects() { return m_projects; }
-    inline Model* getModel(int modelId) { return m_models.value(modelId); }
+    inline GraphPlugin* getGraph(const QString& graphId) { return m_graphs.value(graphId); }
+    inline ModelPlugin* getModel(const QString& modelId) { return m_models.value(modelId); }
     inline Project* getProject(int projId) { return m_projects.value(projId); }
     inline const QStringList& getGeneralProperties() { return m_generalProperties; }
 
 private:
     ProcessesMgr* m_processesMgr;
-    QHash<int, Model*> m_models;
+
+    QHash<QString, GraphPlugin*> m_graphs;
+    QHash<QString, ModelPlugin*> m_models;
     QHash<int, Project*> m_projects;
 
     // lets build a list with the name of the essential properties
