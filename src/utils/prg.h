22a17a0214e274abd86ea19334c6da9cb6c0e45a
Marcos CARDINOT <mcardinot@gmail.com>
2017-02-07 15:47:22 +0000

    fixing the PRG class

--- src/utils/prg.h
+++ src/utils/prg.h
@@ -6,25 +6,35 @@
 #ifndef PRG_H
 #define PRG_H
 
-#include "QVariant"
+#include <random>
 
 class PRG
 {
 public:
-    explicit PRG(int seed);
+    explicit PRG(int seed):m_mteng(seed), m_zeroOne(0, 1) {}
 
     // Generate a random double [0, 1)
-    double randD();
-    // Generate a random double [min, max)
-    double randD(QVariant min, QVariant max);
+    double randD() { return m_zeroOne(m_mteng); }
 
-    // Generate a random integer [0, max)
-    int randI(int max);
-    // Generate a random integer [min, max)
-    int randI(int min, int max);
+    // Generate a random double [min, max)
+    double randD(double min, double max) {
+        std::uniform_real_distribution<double> dis(min, max);
+        return dis(m_mteng);
+    }
+    // Generate a random double [0, max)
+    double randD(double max) { return randD(0.0, max); }
+
+    // Generate a random int [min, max)
+    int randI(int min, int max) {
+        std::uniform_int_distribution<int> dis(min, max);
+        return dis(m_mteng);
+    }
+    // Generate a random int [0, max)
+    int randI(int max) { return randI(0, max); }
 
 private:
-    const int m_prg;
+    std::mt19937 m_mteng; //  Mersenne Twister engine
+    std::uniform_real_distribution<double> m_zeroOne;
 };
 
 #endif // PRG_H
