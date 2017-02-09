0a5235a5ad54c8e55580cccbd3665637c15a86a6
Marcos CARDINOT <mcardinot@gmail.com>
2017-02-09 01:42:49 +0000

    main class - remove prg stuff

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,6 @@
  * @author Marcos Cardinot <mcardinot@gmail.com>
  */
 
-// std stuff, used in PRNG
-#include <functional>
-#include <iostream>
-#include <random>
-#include <time.h>
-
 #include <QApplication>
 #include <QCoreApplication>
 #include <QDebug>
@@ -17,8 +11,6 @@
 
 #include "core/mainapp.h"
 #include "gui/maingui.h"
-
-static std::mt19937 _mt_eng_; //  Mersenne Twister engine
 
 QCoreApplication* createApp(int& argc, char* argv[])
 {
@@ -38,15 +30,6 @@ int main(int argc, char* argv[])
              << "* Copyright (C) 2016 - Marcos Cardinot          *\n"
              << "* @author Marcos Cardinot <mcardinot@gmail.com> *\n"
              << "*************************************************\n";
-
-    // seed PRG
-    // http://stackoverflow.com/a/15509942
-    std::array<int, 624> seed_data;
-    std::random_device r;
-    std::generate_n(seed_data.data(), seed_data.size(), std::ref(r));
-    std::seed_seq seq(std::begin(seed_data), std::end(seed_data));
-    _mt_eng_ = std::mt19937(seq);
-    qsrand(time(NULL)); // just in case... =D
 
     // init application
     MainApp mainApp;
