764345832577a7dbfefc9ac0352dcfed02cf43a9
Marcos CARDINOT <mcardinot@gmail.com>
2017-02-11 18:34:43 +0000

    plugin - updating our fake model

--- plugins/models/hopd/model.cpp
+++ plugins/models/hopd/model.cpp
@@ -2,70 +2,19 @@
 
 #include "model.h"
 
-Model::Model()
-    : m_graph(NULL)
-    , m_b(1.6)
+ModelHOPD::ModelHOPD()
+    : m_b(1.6)
     , m_l(0.3)
 {
 }
 
-Model::~Model()
-{
-    delete m_graph;
-    m_graph = NULL;
-}
-
-bool Model::init(AbstractGraph* graph, const QVariantHash& modelParams)
+bool ModelHOPD::init(const QVariantHash& modelParams)
 {
     return true;
 }
 
-bool Model::algorithmStep()
+bool ModelHOPD::algorithmStep()
 {
     qDebug() << "HOPD";
     return false;
-}
-
-AbstractAgent* Model::newDefaultAgent() const
-{
-    AbstractAgent* a = new AbstractAgent();
-    a->setProperty("strategy", 0);
-    a->setProperty("probToA", 0.0);
-    return a;
-}
-
-QVariantHash Model::paramsSpace() const
-{
-    QVariantHash h;
-    //
-    // agent definition
-    //
-    // probability to turn to abstainer
-    h.insert("probToA", "[0,1]");
-    // possible strategies for this model
-    // 0 (cooperator), 1 (defector) or 2 (abstainer)
-    h.insert("strategy", "{0,1,2}");
-
-    //
-    // model stuff
-    //
-    h.insert("graphType", "{mooreGrid}");
-    h.insert("b", "[1,2]");
-    h.insert("l", "[0,1]");
-    return h;
-}
-
-QVariantHash Model::getModelParams() const
-{
-    QVariantHash h;
-    h.insert("b", m_b);   // temptation to defect
-    h.insert("l", m_l);   // loner's payoff
-    return h;
-}
-
-QVariantHash Model::getInspectorParams() const
-{
-    QVariantHash h;
-    // TODO
-    return h;
 }
--- plugins/models/hopd/model.h
+++ plugins/models/hopd/model.h
@@ -9,46 +9,20 @@
 #include <QObject>
 #include <QVariantHash>
 
-#include "../src/core/abstractgraph.h"
-#include "../src/core/imodel.h"
+#include "core/abstractmodel.h"
 
-class Model: public IModel
+class ModelHOPD: public AbstractModel
 {
-    Q_OBJECT
-    Q_INTERFACES(IModel)
-
 public:
-    Model();
-    ~Model();
-
-    bool init(AbstractGraph* graph, const QVariantHash& modelParams);
-
+    ModelHOPD();
+    bool init(const QVariantHash& modelParams);
     bool algorithmStep();
 
-    QVariantHash paramsSpace() const;
-    AbstractAgent *newDefaultAgent() const;
-    QVariantHash getModelParams() const;
-    QVariantHash getInspectorParams() const;
-
 private:
-    AbstractGraph* m_graph;
-
     double m_b;
     double m_l;
 };
 
-// This class will be loaded as a plugin.
-class PMModel: public QObject, public IPluginModel
-{
-    Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.evoplex.IPluginModel")
-    Q_INTERFACES(IPluginModel)
-
-public:
-    IModel* create() { return qobject_cast<IModel*>(new Model());  }
-    const QString& name() { return "hopd"; }
-    const QString& description() { return "optional prisoner's dilemma"; }
-    const QString& author() { return "author"; }
-};
+REGISTER_MODEL(ModelHOPD)
 
 #endif // HOPD_H
