a63aa5c683291d155dfc463b268a60ccafb4b57a
Marcos CARDINOT <mcardinot@gmail.com>
2017-02-13 03:23:50 +0000

    let's use a dark theme

--- src/main.cpp
+++ src/main.cpp
@@ -39,6 +39,24 @@ int main(int argc, char* argv[])
     if (app) {
         // start GUI application
         app->setStyle(QStyleFactory::create("Fusion"));
+
+        QPalette darkPalette;
+        darkPalette.setColor(QPalette::Window, QColor(53,53,53));
+        darkPalette.setColor(QPalette::WindowText, Qt::white);
+        darkPalette.setColor(QPalette::Base, QColor(25,25,25)); // d
+        darkPalette.setColor(QPalette::AlternateBase, QColor(53,53,53));
+        darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+        darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+        darkPalette.setColor(QPalette::Text, Qt::white);
+        darkPalette.setColor(QPalette::Button, QColor(53,53,53));
+        darkPalette.setColor(QPalette::ButtonText, Qt::white);
+        darkPalette.setColor(QPalette::BrightText, Qt::red);
+        darkPalette.setColor(QPalette::Link, QColor(42, 130, 218)); // add
+        darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218)); // d
+        darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+        app->setPalette(darkPalette);
+        app->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+
         MainGUI gui(&mainApp);
         gui.show();
         result = app->exec();
