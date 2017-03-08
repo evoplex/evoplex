375722be6556fa632b3504d10050da2038d1c843
Marcos CARDINOT <mcardinot@gmail.com>
2017-03-08 08:56:40 +0000

    constants - add plugin stuff

--- src/utils/constants.h
+++ src/utils/constants.h
@@ -18,6 +18,18 @@
 #define GENERAL_ATTRIBUTE_SEED "seed"                   // seed of the PRG
 #define GENERAL_ATTRIBUTE_STOPAT "stopAt"               // hard stop condition, ie., last simulation step
 #define GENERAL_ATTRIBUTE_TRIALS "trials"               // number of times the experiment has to be repeated
-#define GENERAL_ATTRIBUTE_ALLOWEDGRAPHS "allowedGraphs" // graphIds allowed in the model
+
+// Plugin stuff
+#define PLUGIN_ATTRIBUTE_UID "uid"                      // the plugin's unique id
+#define PLUGIN_ATTRIBUTE_AUTHOR "author"                // authors' name
+#define PLUGIN_ATTRIBUTE_NAME "name"                    // plugin's name
+#define PLUGIN_ATTRIBUTE_DESCRIPTION "description"      // plugin's description
+
+#define PLUGIN_ATTRIBUTE_GRAPHSPACE "graphAttributesSpace"  // domain of the graph's attributes
+
+#define PLUGIN_ATTRIBUTE_AGENTSPACE "agentAttributesSpace"  // domain of the agent's attributes
+#define PLUGIN_ATTRIBUTE_MODELSPACE "modelAttributesSpace"  // domain of the model's attributes
+#define PLUGIN_ATTRIBUTE_SUPPORTEDGRAPHS "supportedGraphs"  // graphIds allowed in the model
+
 
 #endif // CONSTANTS_H
