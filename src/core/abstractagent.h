0852f4fbda631d2731d16710aab164b2c755b84e
Marcos CARDINOT <mcardinot@gmail.com>
2017-06-29 09:44:09 +0100

    abstractagent - add 2d coordinates

--- src/core/abstractagent.h
+++ src/core/abstractagent.h
@@ -13,7 +13,9 @@ class AbstractAgent
 {
 public:
     explicit AbstractAgent() {}
-    explicit AbstractAgent(QVariantHash attr): m_attributes(attr) { m_attributes.squeeze(); }
+    explicit AbstractAgent(QVariantHash attr)
+        : m_attributes(attr), m_x(0), m_y(0) { m_attributes.squeeze(); }
+
     ~AbstractAgent() {}
 
     inline AbstractAgent clone() {
@@ -27,12 +29,23 @@ public:
     inline const QVariant getAttribute(const QString& name) const {
         return m_attributes.value(name);
     }
-
     inline void setAttribute(const QString& name, const QVariant& value) {
         m_attributes.insert(name, value);
     }
 
+    inline const int getId() const { return m_id; }
+    inline void setId(int id) { m_id = id; }
+
+    inline const int getX() const { return m_x; }
+    inline void setX(int x) { m_x = x; }
+    inline const int getY() const { return m_y; }
+    inline void setY(int y) { m_y = y; }
+    inline void setCoords(int x, int y) { setX(x); setY(y); }
+
 private:
+    int m_id;
+    int m_x;
+    int m_y;
     QVariantHash m_attributes;
 };
 
