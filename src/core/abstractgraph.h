652ae3f2c59e1321a6a2d86cfd9887f98453d0ed
Marcos CARDINOT <mcardinot@gmail.com>
2017-09-18 05:16:30 +0100

    AbstractGraph - several fixes

--- src/core/abstractgraph.h
+++ src/core/abstractgraph.h
@@ -12,31 +12,74 @@
 #include <QtPlugin>
 
 #include "core/abstractagent.h"
+#include "core/edge.h"
+#include "utils/prg.h"
 
-class Edge
+typedef QHash<AbstractAgent*, Edges*> AdjacencyList;
+
+class AbstractBaseGraph
 {
-public:
-    explicit Edge() {}
-    explicit Edge(const AbstractAgent& neighbour): m_neighbour(neighbour) {}
-    explicit Edge(const AbstractAgent& neighbour, QVariantHash attributes): m_neighbour(neighbour), m_attributes(attributes) {}
+    friend class Experiment;
 
-    inline const QVariant getAttribute(const QString& name) { return m_attributes.value(name); }
-    inline AbstractAgent getNeighbour() { return m_neighbour; }
-    inline void setNeighbour(AbstractAgent neighbour) { m_neighbour = neighbour; }
+public:
+    inline const QString& getGraphName() const { return m_graphName; }
+    inline const Agents& getAgents() const { return m_agents; }
+    inline const AdjacencyList& getAdjacencyList() const { return m_adjacencyList; }
+    inline const int agentsSize() const { return m_agentsSize; }
+
+    inline AbstractAgent* getAgent(int id) const { return m_agents.at(id); }
+    inline AbstractAgent* getRandomAgent() const {
+        return m_agents.at(m_prg->randI(m_agentsSize));
+    }
+
+protected:
+    explicit AbstractBaseGraph(const QString& name): m_graphName(name) {}
+
+    virtual ~AbstractBaseGraph() {
+        setAdjacencyList(AdjacencyList());
+        qDeleteAll(m_agents);
+        m_agents.clear();
+        m_agents.squeeze();
+    }
+
+    inline void setAdjacencyList(AdjacencyList adjacencylist) {
+        // delete all edges
+        AdjacencyList::iterator it = m_adjacencyList.begin();
+        while (it != m_adjacencyList.end()) {
+            qDeleteAll(it.value()->begin(), it.value()->end());
+            it.value()->clear();
+            it.value()->squeeze();
+            ++it;
+        }
+        // clear the QHash but do not touch the agents
+        m_adjacencyList.clear();
+        m_adjacencyList.squeeze();
+        // assign to the new adjacency list
+        m_adjacencyList = adjacencylist;
+    }
+
 private:
-    AbstractAgent m_neighbour;
-    QVariantHash m_attributes;
+    const QString m_graphName;
+    Agents m_agents;
+    PRG* m_prg;
+    AdjacencyList m_adjacencyList;
+    int m_agentsSize;
+
+    // takes the ownership of the agents
+    inline void setup(PRG* prg, Agents agents) {
+        Q_ASSERT(m_prg && agents.isEmpty()); // make sure it'll be called only once
+        m_prg = prg;
+        m_agents = agents;
+        m_agentsSize = agents.size();
+    }
 };
 
-typedef QVector<Edge> Neighbours;  // neighbourhood of one agent
-typedef QHash<int, Neighbours> AdjacencyList;
-typedef QHash<int, AbstractAgent> Population;
 
-class AbstractGraph
+class AbstractGraph : public AbstractBaseGraph
 {
 public:
     // constructor
-    explicit AbstractGraph(const QString& name): m_graphName(name) {}
+    explicit AbstractGraph(const QString& name): AbstractBaseGraph(name) {}
 
     // Provide the destructor to keep compilers happy.
     virtual ~AbstractGraph() {}
@@ -44,29 +87,14 @@ public:
     // Initializes the graph object.
     // This method is called once when a new graph object is being created.
     // It is usually used to validate the graphParams and the set of agents.
-    virtual bool init(const QVector<AbstractAgent>& agents, const QVariantHash& graphParams) = 0;
+    virtual bool init(const QVariantHash& graphParams) = 0;
 
-    // Reset the neighbourhood of all agents to the original structure.
-    virtual void resetNetwork() = 0;
-
-    // This method is used to introduce spatial coordinates for each agent.
-    // It is mainly used by the GUI when it wants to draw the graph.
-    virtual void resetCoordinates() = 0;
+    // Resets the graph object to the original state.
+    virtual void reset() = 0;
 
     // return the current value of all graph parameters (if any)
     // eg., height, width ...
     virtual QVariantHash getGraphParams() const = 0;
-
-    // getters
-    inline const QString& getGraphName() const { return m_graphName; }
-    inline const AbstractAgent getAgent(int id) const { return m_population.value(id); }
-    inline const Neighbours getNeighbours(int id) const { return m_adjacencyList.value(id); }
-    inline const Population& getPopulation() const { return m_population; }
-
-protected:
-    const QString m_graphName;
-    AdjacencyList m_adjacencyList;
-    Population m_population;
 };
 
 
