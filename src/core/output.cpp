223c5e11cbd38bc210dba41f5e504a110cd358a3
Marcos CARDINOT <mcardinot@gmail.com>
2017-11-14 02:23:21 +0000

    Output class - finished

--- src/core/output.cpp
+++ src/core/output.cpp
@@ -11,52 +11,41 @@
 
 namespace evoplex {
 
-DefaultOutput::DefaultOutput(Function f, Entity e, const QString& attrName, int attrIdx,
-                             const Values& inputs, bool enableCache)
-    : Output(enableCache)
+DefaultOutput::DefaultOutput(Function f, Entity e, const QString& attrName, int attrIdx, Values inputs)
+    : Output(inputs)
     , m_func(f)
     , m_entity(e)
     , m_attrName(attrName)
     , m_attrIdx(attrIdx)
-    , m_inputs(inputs)
 {
 }
 
-Values DefaultOutput::doOperation(const AbstractModel* model)
+void DefaultOutput::doOperation(const AbstractModel* model)
 {
-    Values vals;
-
+    Values allValues;
     switch (m_func) {
     case F_Count:
         if (m_entity == E_Agents)
-            vals = Stats::count(model->graph()->agents(), m_attrIdx, m_header);
+            allValues = Stats::count(model->graph()->agents(), m_attrIdx, m_inputs);
         else
-            vals = Stats::count(model->graph()->edges(), m_attrIdx, m_header);
+            allValues = Stats::count(model->graph()->edges(), m_attrIdx, m_inputs);
         break;
     default:
         qFatal("doOperation() invalid function!");
         break;
     }
-
-    if (m_cacheEnabled) {
-        m_cache.emplace_back(vals);
-    }
-    return vals;
+    updateCaches(allValues);
 }
 
 QString DefaultOutput::printableHeader()
 {
-    QString func;
-    if (m_func == F_Count) func = "count";
-    else func = "invalid";
-
     QString prefix = QString("%1_%2_%3_")
-            .arg(func)
+            .arg(stringFromFunc(m_func))
             .arg((m_entity == E_Agents ? "agents" : "edges"))
             .arg(m_attrName);
 
     QString ret;
-    for (Value val : m_header) {
+    for (Value val : m_inputs) {
         ret += prefix + val.toQString() + ",";
     }
     ret.chop(1);
@@ -65,63 +54,109 @@ QString DefaultOutput::printableHeader()
 
 bool DefaultOutput::operator==(const Output* output)
 {
-    DefaultOutput* other = dynamic_cast<DefaultOutput*>(ouput);
+    const DefaultOutput* other = dynamic_cast<const DefaultOutput*>(output);
     if (!other) return false;
     if (m_func != other->function()) return false;
     if (m_entity != other->entity()) return false;
-    // TODO
+    if (m_attrIdx != other->attrIdx()) return false;
+    return true;
 }
 
 /*******************************************************/
 /*******************************************************/
 
-CustomOutput::CustomOutput(const std::vector<std::string>& header, bool enableCache)
-        : Output(enableCache)
-        , m_header(header)
+CustomOutput::CustomOutput(Values inputs)
+        : Output(inputs)
 {
 }
 
-Values CustomOutput::doOperation(const AbstractModel* model)
+void CustomOutput::doOperation(const AbstractModel* model)
 {
-    Values vals = model->customOutputs(m_header);
-    if (m_cacheEnabled) {
-        m_cache.emplace_back(vals);
-    }
-    return vals;
+    updateCaches(model->customOutputs(m_inputs));
 }
 
 QString CustomOutput::printableHeader()
 {
     QString ret;
-    for (std::string h : m_header) {
-        ret += QString::fromStdString(h) + ",";
+    for (Value h : m_inputs) {
+        ret += h.toQString() + ",";
     }
     ret.chop(1);
     return ret;
 }
 
-/*******************************************************/
-/*******************************************************/
-
-Output::Output(bool enableCache)
-    : m_cacheEnabled(enableCache)
+bool CustomOutput::operator==(const Output* output)
 {
+    const CustomOutput* other = dynamic_cast<const CustomOutput*>(output);
+    if (!other) return false;
+    if (m_inputs.size() != other->inputs().size()) return false;
+    for (int i = 0; i < m_inputs.size(); ++i) {
+        if (m_inputs.at(i) != other->inputs().at(i))
+            return false;
+    }
+    return true;
 }
 
-void Output::flushCache()
+/*******************************************************/
+/*******************************************************/
+
+Output::Output(Values inputs)
 {
-    m_cache.clear();
-    m_cache.shrink_to_fit();
+    addCache(inputs);
+}
+
+const int Output::addCache(Values inputs)
+{
+    m_inputs.insert(m_inputs.end(), inputs.begin(), inputs.end());
+    Cache c;
+    c.inputs = inputs;
+    m_caches.emplace_back(c);
+    return m_caches.size() - 1;
+}
+
+void Output::updateCaches(const Values& allValues)
+{
+    if (m_caches.size() == 0) {
+        return;
+    }
+
+    // the first cache will always hold the initial input
+    Cache& cache = m_caches.at(0);
+    Values vals;
+    vals.insert(vals.end(), allValues.begin(), allValues.begin() + cache.inputs.size());
+    if (cache.rows.empty()) cache.last = cache.rows.before_begin();
+    cache.last = cache.rows.emplace_after(cache.last, vals);
+
+    for (int cacheId = 1; cacheId < m_caches.size(); ++cacheId) {
+        Cache& cache = m_caches.at(cacheId);
+        Values vals;
+        vals.reserve(cache.inputs.size());
+        for (Value input : cache.inputs) {
+            int col = std::find(m_inputs.begin(), m_inputs.end(), input) - m_inputs.begin();
+            Q_ASSERT(col != m_inputs.size()); // input must exist
+            vals.emplace_back(allValues.at(col));
+        }
+        if (cache.rows.empty()) cache.last = cache.rows.before_begin();
+        cache.last = cache.rows.emplace_after(cache.last, vals);
+    }
 }
 
 std::vector<Output*> Output::parseHeader(const QStringList& header, const Attributes& agentAttrMin,
                                          const Attributes& edgeAttrMin, QString& errorMsg)
 {
     std::vector<Output*> outputs;
-    std::vector<std::string> customHeader;
+    std::vector<Value> customHeader;
     for (QString h : header) {
         if (h.startsWith("custom_")) {
-            customHeader.emplace_back(h.remove("custom_").toStdString());
+            h.remove("custom_");
+            if (h.isEmpty()) {
+                errorMsg = "[OutputHeader] invalid header! Custom function cannot be empty.\n";
+                qWarning() << errorMsg;
+                qDeleteAll(outputs);
+                outputs.clear();
+                return outputs;
+            }
+            customHeader.emplace_back(h);
             continue;
         }
 
@@ -165,7 +200,7 @@ std::vector<Output*> Output::parseHeader(const QStringList& header, const Attrib
             return outputs;
         }
 
-        std::vector<Value> attrHeader;
+        std::vector<Value> attrHeader; //inputs
         attrHeaderStr.removeFirst();
         for (QString valStr : attrHeaderStr) {
             Value val = Utils::valueFromString(entityAttrMin.value(attrIdx).type, valStr);
@@ -179,11 +214,19 @@ std::vector<Output*> Output::parseHeader(const QStringList& header, const Attrib
             attrHeader.emplace_back(val);
         }
 
-        outputs.emplace_back(new DefaultOutput(func, entity, entityAttrMin.name(attrIdx), attrIdx, attrHeader, false));
+        if (attrHeader.empty()) {
+            errorMsg = "[OutputHeader] invalid header! Function cannot be empty.\n";
+            qWarning() << errorMsg;
+            qDeleteAll(outputs);
+            outputs.clear();
+            return outputs;
+        }
+
+        outputs.emplace_back(new DefaultOutput(func, entity, entityAttrMin.name(attrIdx), attrIdx, attrHeader));
     }
 
     if (!customHeader.empty()) {
-        outputs.emplace_back(new CustomOutput(customHeader, false));
+        outputs.emplace_back(new CustomOutput(customHeader));
     }
 
     return outputs;
--- src/core/output.h
+++ src/core/output.h
@@ -6,6 +6,7 @@
 #ifndef OUTPUT_H
 #define OUTPUT_H
 
+#include <forward_list>
 #include <vector>
 
 #include "abstractmodel.h"
@@ -17,83 +18,97 @@ namespace evoplex {
 class Output
 {
 public:
-    explicit Output(bool enableCache = false);
+    explicit Output(Values inputs);
 
-    virtual Values doOperation(const AbstractModel* model) = 0;
+    virtual void doOperation(const AbstractModel* model) = 0;
 
     // Printable header with all columns of this operation separated by commas.
     virtual QString printableHeader() = 0;
 
-    virtual bool operator==(const Output& output) = 0;
+    virtual bool operator==(const Output* output) = 0;
 
     static std::vector<Output*> parseHeader(const QStringList& header,
             const Attributes& agentAttrMin, const Attributes &edgeAttrMin, QString &errorMsg);
 
-    const std::vector<Values> readCache() const { return m_cache; }
-    void flushCache();
+    const int addCache(Values inputs);
+    inline bool isEmpty(int cacheId) const { return m_caches.at(cacheId).rows.empty(); }
+    inline const Values& readFrontRow(int cacheId) const { return m_caches.at(cacheId).rows.front(); }
+    inline void flushFrontRow(int cacheId) { m_caches.at(cacheId).rows.pop_front(); }
 
-    inline bool cacheEnabled() const { return m_cacheEnabled; }
-    inline void setEnableCache(bool enabled) { m_cacheEnabled = enabled; }
+    inline const Values& inputs() const { return m_inputs; }
 
 protected:
-    bool m_cacheEnabled;
-    std::vector<Values> m_cache;
+    struct Cache {
+        Values inputs; // columns
+        std::forward_list<Values> rows;
+        std::forward_list<Values>::iterator last;
+    };
+    std::vector<Cache> m_caches;
+    Values m_inputs;
+
+    // auxiliar method for 'doOperation()'
+    void updateCaches(const Values& allValues);
 };
 
 
 class CustomOutput: public Output
 {
 public:
-    explicit CustomOutput(const std::vector<std::string>& header, bool enableCache);
+    explicit CustomOutput(Values inputs);
 
-    virtual Values doOperation(const AbstractModel* model);
+    virtual void doOperation(const AbstractModel* model);
 
     // Printable header with all columns of this operation separated by commas.
     // Format: "custom_nameDefinedInTheModel"
     virtual QString printableHeader();
 
-    virtual bool operator==(const Output& output);
-
-private:
-    const std::vector<std::string> m_header;
+    virtual bool operator==(const Output* output);
 };
 
 
 class DefaultOutput : public Output
 {
 public:
-    enum Function {
-        F_Invalid,
-        F_Count
-    };
-
     enum Entity {
         E_Agents,
         E_Edges
     };
 
-    explicit DefaultOutput(Function f, Entity e, const QString& attrName, int attrIdx,
-                           const Values& inputs, bool enableCache);
+    enum Function {
+        F_Invalid,
+        F_Count
+    };
+    static std::vector<QString> availableFunctions() {
+        return {"count"};
+    }
+    static Function funcFromString(QString f) {
+        if (f == "count") return F_Count;
+        return F_Invalid;
+    }
+    static QString stringFromFunc(Function f) {
+        if (f == F_Count) return "count";
+        return "invalid";
+    }
 
-    virtual Values doOperation(const AbstractModel* model);
+    explicit DefaultOutput(Function f, Entity e, const QString& attrName, int attrIdx, Values inputs);
+
+    virtual void doOperation(const AbstractModel* model);
 
     // Printable header with all columns of this operation separated by commas.
     // Format: "function_entity_attrName_value"
     virtual QString printableHeader();
 
-    virtual bool operator==(const Output& output);
+    virtual bool operator==(const Output* output);
 
     inline Function function() const { return m_func; }
     inline Entity entity() const { return m_entity; }
     inline int attrIdx() const { return m_attrIdx; }
-    inline const Values& inputs() const { return m_inputs; }
 
 private:
     const Function m_func;
     const Entity m_entity;
     const QString m_attrName;
     const int m_attrIdx;
-    const std::vector<Value> m_inputs;
 };
 
 }
