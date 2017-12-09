67c6d11662d1a5f6c5b5daae157fc042a3b3f780
Marcos CARDINOT <mcardinot@gmail.com>
2017-12-09 04:20:26 +0000

    MainApp - minor fixes

--- src/core/mainapp.h
+++ src/core/mainapp.h
@@ -37,17 +37,17 @@ public:
 
     void closeProject(int projId);
 
-    inline FileMgr* getFileMgr() { return m_fileMgr; }
-    inline ExperimentsMgr* getExperimentsMgr() { return m_experimentsMgr; }
-    inline const QHash<QString, GraphPlugin*>& getGraphs() { return m_graphs; }
-    inline const QHash<QString, ModelPlugin*>& getModels() { return m_models; }
-    inline const QHash<int, Project*>& getProjects() { return m_projects; }
+    inline FileMgr* getFileMgr() const { return m_fileMgr; }
+    inline ExperimentsMgr* getExperimentsMgr() const { return m_experimentsMgr; }
+    inline const QHash<QString, GraphPlugin*>& getGraphs() const { return m_graphs; }
+    inline const QHash<QString, ModelPlugin*>& getModels() const { return m_models; }
+    inline const QHash<int, Project*>& getProjects() const { return m_projects; }
 
-    inline const GraphPlugin* getGraph(const QString& graphId) { return m_graphs.value(graphId, nullptr); }
-    inline const ModelPlugin* getModel(const QString& modelId) { return m_models.value(modelId, nullptr); }
-    inline Project* getProject(int projId) { return m_projects.value(projId); }
+    inline const GraphPlugin* getGraph(const QString& graphId) const { return m_graphs.value(graphId, nullptr); }
+    inline const ModelPlugin* getModel(const QString& modelId) const { return m_models.value(modelId, nullptr); }
+    inline Project* getProject(int projId) const { return m_projects.value(projId); }
 
-    inline const AttributesSpace& getGeneralAttrSpace() { return m_generalAttrSpace; }
+    inline const AttributesSpace& getGeneralAttrSpace() const { return m_generalAttrSpace; }
 
 private:
     FileMgr* m_fileMgr;
@@ -62,14 +62,6 @@ private:
     // lets build a hash with the name and space of the essential parameters
     // it is important to validate the contents of csv files
     AttributesSpace m_generalAttrSpace;
-
-    // load graph from a .so file
-    // return the graph uid
-    bool loadGraphPlugin(QObject* instance, QJsonObject& metaData);
-
-    // load model from a .so file
-    // return the model uid
-    bool loadModelPlugin(QObject* instance, QJsonObject& metaData);
 };
 }
 
