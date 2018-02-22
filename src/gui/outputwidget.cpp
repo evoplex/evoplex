9b8dc18181e6c00ce6b490a9da01318559d4df1c
Marcos CARDINOT <mcardinot@gmail.com>
2018-02-22 17:35:31 +0000

    outputwidget - adjust to the new Output class

--- src/gui/outputwidget.cpp
+++ src/gui/outputwidget.cpp
@@ -3,6 +3,7 @@
  * @author Marcos Cardinot <mcardinot@gmail.com>
  */
 
+#include <QCloseEvent>
 #include <QMessageBox>
 
 #include "outputwidget.h"
@@ -10,25 +11,67 @@
 
 namespace evoplex {
 
-OutputWidget::OutputWidget(const ModelPlugin* modelPlugin, QWidget *parent)
+OutputWidget::OutputWidget(const ModelPlugin* modelPlugin, const std::vector<int>& trialIds,
+                           QWidget *parent, const std::vector<Cache*>& init)
     : QDialog(parent)
     , m_ui(new Ui_OutputWidget)
     , m_modelPlugin(modelPlugin)
+    , m_trialIds(trialIds)
+    , m_hasChanges(false)
 {
+    setWindowModality(Qt::ApplicationModal);
     m_ui->setupUi(this);
 
+    connect(m_ui->bBox, &QDialogButtonBox::accepted, [this]() { slotClose(false); });
+    connect(m_ui->bBox, &QDialogButtonBox::rejected, [this]() { slotClose(true); });
+
     for (QString funcName : DefaultOutput::availableFunctions()) {
-        m_ui->func->addItem(funcName, 0); // default function
+        m_ui->func->addItem(funcName, DefaultFunc);
     }
     for (QString funcName : m_modelPlugin->customOutputs()) {
-        m_ui->func->addItem(funcName, 1); // custom function
+        m_ui->func->addItem(funcName, CustomFunc);
     }
+
+    m_ui->entityAgent->setChecked(true);
+    slotEntityChanged(true); // init
+    m_ui->entityEdge->setDisabled(modelPlugin->edgeAttrNames().empty());
+
     connect(m_ui->add, SIGNAL(clicked(bool)), SLOT(slotAdd()));
     connect(m_ui->func, SIGNAL(currentIndexChanged(int)), SLOT(slotFuncChanged(int)));
     connect(m_ui->entityAgent, SIGNAL(toggled(bool)), SLOT(slotEntityChanged(bool)));
 
-    m_ui->entityAgent->setChecked(true);
-    m_ui->entityEdge->setDisabled(modelPlugin->edgeAttrNames().empty());
+    m_ui->table->horizontalHeader()->setSectionResizeMode(0, QHeaderView::ResizeToContents);
+    m_ui->table->horizontalHeader()->setSectionResizeMode(1, QHeaderView::Stretch);
+    m_ui->table->horizontalHeader()->setSectionResizeMode(2, QHeaderView::Stretch);
+    m_ui->table->horizontalHeader()->setSectionResizeMode(3, QHeaderView::Stretch);
+    m_ui->table->horizontalHeader()->setSectionResizeMode(4, QHeaderView::Stretch);
+
+    // init table
+    for (const Cache* cache : init) {
+        const int rootId = m_ui->table->rowCount();
+        DefaultOutputSP df = std::dynamic_pointer_cast<DefaultOutput>(cache->output());
+        if (df) {
+            const QString entityStr = df->entity() == DefaultOutput::E_Agents
+                                    ? m_ui->entityAgent->text() : m_ui->entityEdge->text();
+
+            for (Value input : cache->inputs()) {
+                RowInfo rowInfo;
+                rowInfo.id = m_ui->table->rowCount();
+                rowInfo.equalToId = rowInfo.id == rootId ? -1 : rootId;
+                insertRow(rowInfo, df->functionStr(), DefaultFunc, entityStr, df->entity(),
+                          df->valueSpace()->attrName(), input.toQString());
+            }
+        } else if (std::dynamic_pointer_cast<CustomOutput>(cache->output())) {
+            for (Value func : cache->inputs()) {
+                RowInfo rowInfo;
+                rowInfo.id = m_ui->table->rowCount();
+                rowInfo.equalToId = rowInfo.id == rootId ? -1 : rootId;
+                insertRow(rowInfo, func.toQString(), CustomFunc);
+            }
+        } else {
+            qFatal("[OutputWidget] : error! invalid Output object.");
+        }
+    }
 }
 
 OutputWidget::~OutputWidget()
@@ -36,20 +79,83 @@ OutputWidget::~OutputWidget()
     delete m_ui;
 }
 
-void OutputWidget::hideEvent(QHideEvent* event)
+void OutputWidget::closeEvent(QCloseEvent*)
 {
-    if (!m_newOutputs.empty()) {
-        emit (closed(m_newOutputs));
-        m_newOutputs.clear();
+    slotClose(true);
+}
+
+void OutputWidget::slotClose(bool canceled)
+{
+    if (!m_hasChanges) {
+        reject(); // reject() to let other know that nothing was changed
+        return;
+    } else if (canceled) {
+        int res = QMessageBox::warning(this, "Evoplex",
+                    "Are you sure you want to want to discard your changes?",
+                    QMessageBox::Yes, QMessageBox::Cancel);
+        if (res == QMessageBox::Yes) {
+            reject();
+            return;
+        } else {
+            return;
+        }
     }
-    QWidget::hideEvent(event);
+
+    Q_ASSERT(m_allCaches.empty());
+
+    for (int row = 0; row < m_ui->table->rowCount(); ++row) {
+        QString funcStr = m_ui->table->item(row, 1)->text();
+        FuncType funcType = (FuncType) m_ui->table->item(row, 1)->data(Qt::UserRole).toInt();
+        DefaultOutput::Entity entity = (DefaultOutput::Entity) m_ui->table->item(row, 2)->data(Qt::UserRole).toInt();
+        QString attr = m_ui->table->item(row, 3)->text();
+        QString inputStr = m_ui->table->item(row, 4)->text();
+        RowInfo rinfo = m_ui->table->item(row, 4)->data(Qt::UserRole).value<RowInfo>();
+
+        const ValueSpace* entityValSpace;
+        if (entity == DefaultOutput::E_Agents) {
+            entityValSpace = m_modelPlugin->agentAttrSpace().value(attr);
+        } else {
+            entityValSpace = m_modelPlugin->edgeAttrSpace().value(attr);
+        }
+
+        if (rinfo.equalToId == -1) {
+            Cache* cache = nullptr;
+            if (funcType == DefaultFunc) {
+                Value input = entityValSpace->validate(inputStr);
+                DefaultOutput::Function func = DefaultOutput::funcFromString(funcStr);
+                Q_ASSERT(func != DefaultOutput::F_Invalid && input.isValid());
+                OutputSP newOutput (new DefaultOutput(func, entity, entityValSpace));
+                cache = newOutput->addCache({input}, m_trialIds);
+            } else {
+                OutputSP newOutput (new CustomOutput());
+                cache = newOutput->addCache({Value(funcStr)}, m_trialIds);
+            }
+            m_allCaches.insert({rinfo.id, cache});
+        } else {
+            OutputSP existingOutput = m_allCaches.at(rinfo.equalToId)->output();
+            Value input;
+            if (funcType == DefaultFunc) {
+                input = entityValSpace->validate(inputStr);
+            } else {
+                input = Value(funcStr);
+            }
+            m_allCaches.insert({rinfo.id, existingOutput->addCache({input}, m_trialIds)});
+        }
+    }
+
+    if (m_allCaches.empty()) {
+        reject();
+    } else {
+        accept();
+    }
 }
 
 void OutputWidget::slotFuncChanged(int idx)
 {
     m_ui->input->clear();
-    m_ui->attr->setEnabled(m_ui->func->itemData(idx) == 0);
-    m_ui->input->setEnabled(m_ui->func->itemData(idx) == 0);
+    bool dfFunc = m_ui->func->itemData(idx) == DefaultFunc;
+    m_ui->attr->setEnabled(dfFunc);
+    m_ui->input->setEnabled(dfFunc);
 }
 
 void OutputWidget::slotEntityChanged(bool isAgent)
@@ -57,10 +163,14 @@ void OutputWidget::slotEntityChanged(bool isAgent)
     m_ui->attr->clear();
     m_ui->input->clear();
     if (isAgent) {
+        m_currEntity = DefaultOutput::E_Agents;
+        m_currEntityStr = m_ui->entityAgent->text();
         for (QString n : m_modelPlugin->agentAttrNames()) {
             m_ui->attr->addItem(n);
         }
     } else {
+        m_currEntity = DefaultOutput::E_Edges;
+        m_currEntityStr = m_ui->entityEdge->text();
         for (QString n : m_modelPlugin->edgeAttrNames()) {
             m_ui->attr->addItem(n);
         }
@@ -70,107 +180,64 @@ void OutputWidget::slotEntityChanged(bool isAgent)
 
 void OutputWidget::slotAdd()
 {
-    int row = m_ui->table->rowCount();
-    QString attrName = m_ui->attr->currentText();
+    m_hasChanges = true;
 
-    Output* output;
-    Value input;
-    if (m_ui->func->currentData().toInt() == 0) { // default output
-        const ValueSpace* valSpace;
-        DefaultOutput::Entity entity;
-        if (m_ui->entityAgent->isChecked()) {
-            entity = DefaultOutput::E_Agents;
-            valSpace = m_modelPlugin->agentAttrSpace().value(attrName);
-        } else {
-            entity = DefaultOutput::E_Edges;
-            valSpace = m_modelPlugin->edgeAttrSpace().value(attrName);
+    RowInfo rowInfo;
+    for (int row = 0; row < m_ui->table->rowCount(); ++row) {
+        if (m_ui->table->item(row, 1)->data(Qt::UserRole) == m_ui->func->currentData() &&
+            m_ui->table->item(row, 1)->text() == m_ui->func->currentText() &&
+            m_ui->table->item(row, 2)->data(Qt::UserRole).toInt() == m_currEntity &&
+            m_ui->table->item(row, 3)->text() == m_ui->attr->currentText())
+        {
+            if (m_ui->table->item(row, 4)->text() == m_ui->input->text()) {
+                QMessageBox::warning(this, "Evoplex", "It's already in the table.");
+                return;
+            }
+            rowInfo.equalToId = m_ui->table->item(row, 0)->text().toInt();
         }
-
-        input = valSpace->validate(m_ui->input->text());
-        if (!input.isValid()) {
-            QMessageBox::warning(this, "Wrong Input", "The 'input' is not valid for the current 'attribute'.");
-            return;
-        }
-
-        DefaultOutput::Function func = DefaultOutput::funcFromString(m_ui->func->currentText());
-        Q_ASSERT(func != DefaultOutput::F_Invalid);
-
-        output = new DefaultOutput(func, entity, valSpace, {input}, m_trialIds);
-
-        m_ui->table->insertRow(row);
-        m_ui->table->setItem(row, 0, new QTableWidgetItem("Default"));
-        m_ui->table->setItem(row, 1, new QTableWidgetItem(m_ui->func->currentText()));
-        m_ui->table->setItem(row, 2, new QTableWidgetItem(entity == DefaultOutput::E_Agents ? "Agent" : "Edge"));
-        m_ui->table->setItem(row, 3, new QTableWidgetItem(m_ui->attr->currentText()));
-        m_ui->table->setItem(row, 4, new QTableWidgetItem(m_ui->input->text()));
-
-    } else { // custom output
-        input = Value(m_ui->func->currentText());
-        output = new CustomOutput({input}, m_trialIds);
-
-        m_ui->table->insertRow(row);
-        m_ui->table->setItem(row, 0, new QTableWidgetItem("Custom"));
-        m_ui->table->setItem(row, 1, new QTableWidgetItem(m_ui->func->currentText()));
     }
 
-    // make sure it is unique
-    for (Output* existing : m_allOutputs) {
-        if (existing && existing->operator ==(output)
-                && existing->allInputs().front().operator ==(output->allInputs().front())) {
-            m_ui->table->removeRow(row);
-            delete output;
-            output = nullptr;
+    const ValueSpace* entityValSpace;
+    if (m_ui->entityAgent->isChecked()) {
+        entityValSpace = m_modelPlugin->agentAttrSpace().value(m_ui->attr->currentText());
+    } else {
+        entityValSpace = m_modelPlugin->edgeAttrSpace().value(m_ui->attr->currentText());
+    }
+
+    if (m_ui->func->currentData().toInt() == DefaultFunc) {
+        if (!entityValSpace->validate(m_ui->input->text()).isValid()) {
+            QMessageBox::warning(this, "Evoplex",
+                                 "The 'input' is not valid for the current 'attribute'.\n"
+                                 "Expected: " + entityValSpace->space());
             return;
         }
     }
 
-    m_newOutputs.emplace_back(output);
-    m_allOutputs.emplace_back(output);
+    rowInfo.id = m_ui->table->rowCount();
+    insertRow(rowInfo, m_ui->func->currentText(), (FuncType) m_ui->func->currentData().toInt(),
+              m_currEntityStr, m_currEntity, m_ui->attr->currentText(), m_ui->input->text());
 }
 
-void OutputWidget::fill(std::vector<Output*> outputs)
+void OutputWidget::insertRow(const RowInfo rowInfo, const QString& funcStr, const FuncType funcType,
+                             QString entityStr, DefaultOutput::Entity entity, QString attr, QString input)
 {
-    m_ui->table->clearContents();
-    Utils::deleteAndShrink(m_allOutputs);
-    Utils::deleteAndShrink(m_newOutputs);
+    m_ui->table->insertRow(rowInfo.id);
 
-    for (Output* output : outputs) {
-        int row = m_ui->table->rowCount();
-        if (dynamic_cast<DefaultOutput*>(output)) {
-            DefaultOutput* df = dynamic_cast<DefaultOutput*>(output);
-            std::vector<int> trialIds(df->trialIds().begin(), df->trialIds().end());
+    m_ui->table->setItem(rowInfo.id, 0, new QTableWidgetItem(QString::number(rowInfo.id)));
 
-            for (Value input : df->allInputs()) {
-                // we create one Output object for each input
-                m_newOutputs.emplace_back(new DefaultOutput(df->function(),
-                        df->entity(), df->valueSpace(), {input}, trialIds));
+    QTableWidgetItem* itemFunc = new QTableWidgetItem(funcStr);
+    itemFunc->setData(Qt::UserRole, funcType);
+    m_ui->table->setItem(rowInfo.id, 1, itemFunc);
 
-                m_ui->table->insertRow(row);
-                m_ui->table->setItem(row, 0, new QTableWidgetItem("Default"));
-                m_ui->table->setItem(row, 1, new QTableWidgetItem(df->functionStr()));
-                m_ui->table->setItem(row, 2, new QTableWidgetItem(df->entity() == DefaultOutput::E_Agents ? "Agent" : "Edge"));
-                m_ui->table->setItem(row, 3, new QTableWidgetItem(df->valueSpace()->attrName()));
-                m_ui->table->setItem(row, 4, new QTableWidgetItem(input.toQString()));
-                ++row;
-            }
-        } else if (dynamic_cast<CustomOutput*>(output)) {
-            CustomOutput* cs = dynamic_cast<CustomOutput*>(output);
-            std::vector<int> trialIds(cs->trialIds().begin(), cs->trialIds().end());
+    QTableWidgetItem* itemEntity = new QTableWidgetItem(entityStr);
+    itemEntity->setData(Qt::UserRole, entity);
+    m_ui->table->setItem(rowInfo.id, 2, itemEntity);
 
-            for (Value func : cs->allInputs()) {
-                // we create one Output object for each input
-                m_newOutputs.emplace_back(new CustomOutput({func}, trialIds));
+    m_ui->table->setItem(rowInfo.id, 3, new QTableWidgetItem(attr));
 
-                m_ui->table->insertRow(row);
-                m_ui->table->setItem(row, 0, new QTableWidgetItem("Custom"));
-                m_ui->table->setItem(row, 1, new QTableWidgetItem(func.toQString()));
-                ++row;
-            }
-        } else {
-            qFatal("[OutputWidget] : error! invalid Output object.");
-        }
-        m_allOutputs.emplace_back(m_newOutputs.back());
-    }
+    QTableWidgetItem* itemInput = new QTableWidgetItem(input);
+    itemInput->setData(Qt::UserRole, QVariant::fromValue(rowInfo));
+    m_ui->table->setItem(rowInfo.id, 4, itemInput);
 }
 
-}
+} // evoplex
--- src/gui/outputwidget.h
+++ src/gui/outputwidget.h
@@ -19,31 +19,54 @@ class OutputWidget : public QDialog
     Q_OBJECT
 
 public:
-    explicit OutputWidget(const ModelPlugin* modelPlugin, QWidget* parent = 0);
+    struct RowInfo {
+        int id = -1;
+        int equalToId = -1; // when Output* is the same, but with different inputs
+    };
+
+    // It creates a new Cache* for each row (which can take only one input).
+    // We don't touch the initial Caches*.
+    // If the user does not make changes, nothing is created and reject() is returned
+    explicit OutputWidget(const ModelPlugin* modelPlugin, const std::vector<int>& trialIds, QWidget* parent,
+                          const std::vector<Cache*> &init = std::vector<Cache*>());
     ~OutputWidget();
 
-    inline void setTrialIds(const std::vector<int>& trialIds) { m_trialIds = trialIds; }
-
-    void fill(std::vector<Output*> outputs);
+    inline std::map<int, Cache*>& caches() { return m_allCaches; }
 
 protected:
-    virtual void hideEvent(QHideEvent* event);
-
-signals:
-    void closed(std::vector<Output*> newOutputs);
+    virtual void closeEvent(QCloseEvent*);
 
 private slots:
     void slotEntityChanged(bool isAgent);
     void slotFuncChanged(int idx);
     void slotAdd();
+    void slotClose(bool canceled);
 
 private:
+    enum FuncType {
+        DefaultFunc,
+        CustomFunc
+    };
+
     Ui_OutputWidget* m_ui;
     const ModelPlugin* m_modelPlugin;
     std::vector<int> m_trialIds;
+    bool m_hasChanges;
 
-    std::vector<Output*> m_allOutputs;
-    std::vector<Output*> m_newOutputs;
+    DefaultOutput::Entity m_currEntity;
+    QString m_currEntityStr;
+
+    // we cannot rely on the row numbers because they might change.
+    // that's why we use Ids in the RowInfo.
+    // also, we need a map to keep the Caches in order
+    std::map<int, Cache*> m_allCaches;
+
+    void insertRow(const RowInfo rowInfo, const QString& funcStr, const FuncType funcType,
+                   QString entityStr="", DefaultOutput::Entity entity=DefaultOutput::E_Agents,
+                   QString attr="", QString input="");
 };
-}
+} // evoplex
+
+Q_DECLARE_METATYPE(evoplex::OutputWidget::RowInfo)
+
 #endif // OUTPUTWIDGET_H
