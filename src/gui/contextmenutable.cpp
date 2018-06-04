11405235a7ee19b531f9e180545a524470e39d3c
Marcos Cardinot <mcardinot@gmail.com>
2018-06-04 22:18:25 +0100

    Adds GNU GPLv3 License

--- src/gui/contextmenutable.cpp
+++ src/gui/contextmenutable.cpp
@@ -1,6 +1,21 @@
 /**
- * Copyright (C) 2016 - Marcos Cardinot
- * @author Marcos Cardinot <mcardinot@gmail.com>
+ *  This file is part of Evoplex.
+ *
+ *  Evoplex is a multi-agent system for networks.
+ *  Copyright (C) 2018 - Marcos Cardinot <marcos@cardinot.net>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <QHBoxLayout>
