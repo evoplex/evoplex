153c85e294433fdabc290de4f8520bbcbca4a4ae
Marcos Cardinot <mcardinot@gmail.com>
2018-06-25 14:41:03 +0100

    logger - fix bug when writting logs on windows + enable msglogger for all modes

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -47,10 +47,11 @@ void Logger::init()
         m_logDir.mkpath(".");
     }
 
-    QString currDate = QDateTime::currentDateTime().toString(Qt::ISODate);
-    m_logFile.setFileName(m_logDir.absoluteFilePath("log_%1.txt").arg(currDate));
+    const QString currDate = QDateTime::currentDateTime().toString("yyyy-MM-dd_HHmmss");
+    const QString logFilename = m_logDir.absoluteFilePath("log_" + currDate + ".txt");
+    m_logFile.setFileName(logFilename);
     if (!m_logFile.open(QIODevice::WriteOnly | QIODevice::Truncate | QIODevice::Text | QIODevice::Unbuffered)) {
-        qWarning() << "ERROR! Unable to write the log file.";
+        qWarning() << "ERROR! Unable to write the log file." << logFilename;
         return;
     }
 
