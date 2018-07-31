3a22486893b0132dd557e335ab7e55fad9e3be0b
EthanPadden <e.padden1@nuigalway.ie>
2018-07-31 19:53:51 +0100

    Improve coding style

--- plugin.cpp
+++ plugin.cpp
@@ -29,27 +29,27 @@ bool MinimalModel::algorithmStep()
             neighbour = ep.edge()->neighbour();
 
             // If the neighbour is alive
-            if(neighbour->attrs().value(m_live).toBool()){
+            if (neighbour->attrs().value(m_live).toBool()) {
                 liveNeighbourCount++;
             }
         }
 
-        if(currentNode->attrs().value(m_live).toBool()){
-            if(liveNeighbourCount < 2){ // Case 1: Underpopulation
+        if (currentNode->attrs().value(m_live).toBool()) {
+            if (liveNeighbourCount < 2) { // Case 1: Underpopulation
                 currentNode->setAttr(m_live_next_state, false);
-            } else if ((liveNeighbourCount == 2)||(liveNeighbourCount == 3)){ // Case 2: Lives to next state
+            } else if ((liveNeighbourCount == 2)||(liveNeighbourCount == 3)) { // Case 2: Lives to next state
                 currentNode->setAttr(m_live_next_state, true);
-            } else if (liveNeighbourCount > 3){ // Case 3: Overpopulation
+            } else if (liveNeighbourCount > 3) { // Case 3: Overpopulation
                 currentNode->setAttr(m_live_next_state, false);
             }
-        } else{
-           if(liveNeighbourCount == 3){ // Case 4: Reproduction
+        } else {
+           if (liveNeighbourCount == 3) { // Case 4: Reproduction
                currentNode->setAttr(m_live_next_state, true);
            }
        }
     }
 
-    for(Nodes::Pair np : nodes()){
+    for (Nodes::Pair np : nodes()) {
         currentNode = np.node();
         nextState = currentNode->attrs().value(m_live_next_state).toBool();
         currentNode->setAttr(m_live, nextState);
