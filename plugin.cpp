391e89708cb3202e54981e9c98a31fae88d9d78a
EthanPadden <e.padden1@nuigalway.ie>
2018-07-31 07:50:56 +0100

    Add changes from remote repos

--- plugin.cpp
+++ plugin.cpp
@@ -9,12 +9,52 @@ namespace evoplex {
 
 bool MinimalModel::init()
 {
+    m_live = AbstractModel::nodes().at(0)->attrs().indexOf("live");
+    m_live_next_state = AbstractModel::nodes().at(0)->attrs().indexOf("live_next_state");
+
     return true;
 }
 
 bool MinimalModel::algorithmStep()
 {
-    return false;
+    int liveNeighbourCount;
+    NodePtr currentNode, neighbour;
+    bool nextState;
+
+    for (Nodes::Pair np : nodes()) {
+        currentNode = np.node();
+        liveNeighbourCount = 0;
+
+        for (Edges::Pair ep : np.node()->outEdges()) {
+            neighbour = ep.edge()->neighbour();
+
+            // If the neighbour is alive
+            if(neighbour->attrs().value(m_live).toBool()){
+                liveNeighbourCount++;
+            }
+        }
+
+        if(currentNode->attrs().value(m_live).toBool()){
+            if(liveNeighbourCount < 2){ // Case 1: Underpopulation
+                currentNode->setAttr(m_live_next_state, false);
+            } else if ((liveNeighbourCount == 2)||(liveNeighbourCount == 3)){ // Case 2: Lives to next state
+                currentNode->setAttr(m_live_next_state, true);
+            } else if (liveNeighbourCount > 3){ // Case 3: Overpopulation
+                currentNode->setAttr(m_live_next_state, false);
+            }
+        } else{
+           if(liveNeighbourCount == 3){ // Case 4: Reproduction
+               currentNode->setAttr(m_live_next_state, true);
+           }
+       }
+    }
+
+    for(Nodes::Pair np : nodes()){
+        currentNode = np.node();
+        nextState = currentNode->attrs().value(m_live_next_state).toBool();
+        currentNode->setAttr(m_live, nextState);
+    }
+    return true;
 }
 
 } // evoplex
--- plugin.h
+++ plugin.h
@@ -14,6 +14,10 @@ class MinimalModel: public AbstractModel
 public:
     bool init() override;
     bool algorithmStep() override;
+
+private:
+    bool m_live;
+    bool m_live_next_state;
 };
 } // evoplex
 #endif // MINIMAL_MODEL_H
