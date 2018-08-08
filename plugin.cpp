9f0068984a0baf1ec297988b67026f49889c1678
EthanPadden <e.padden1@nuigalway.ie>
2018-08-08 15:49:05 +0100

    Clean up code and bug fix

--- plugin.cpp
+++ plugin.cpp
@@ -11,7 +11,7 @@ bool MinimalModel::init()
 {
     m_infectedAttrId = AbstractModel::nodes().at(0)->attrs().indexOf("infected");
 
-    // Initializing global variables
+    // initializing model attribute, which is constant throughout the simulation
     m_prob = attr("prob").toDouble();
 
     return m_infectedAttrId >= 0;
@@ -26,8 +26,10 @@ bool MinimalModel::algorithmStep()
     for (Nodes::Pair np : nodes()) {
         NodePtr currentNode = np.node();
 
-        // if the node is already infected, there's nothing to do
+        // Check if the current node is currently healthy
+        // i.e. if the node is already infected, there's nothing to do
         if (currentNode->attr(m_infectedAttrId).toBool()) {
+            nextInfectedStates.emplace_back(true);
             continue;
         }
 
@@ -43,49 +45,22 @@ bool MinimalModel::algorithmStep()
         }
     }
 
-    // For each node
-//    for (int i = 0; i < m_number_of_nodes; ++i) {
-//        currentNode = AbstractModel::nodes().at(i);
-
-//        // Check if the current node is currently healthy
-//        if (!currentNode->attrs().value(m_infected).toBool()) {
-//            // Select a random neighbour
-//            neighbour = currentNode->randNeighbour(prg);
-
-//            // Check if the neighbour is currently infected
-//            if (neighbour->attrs().value(m_infected).toBool()) {
-//                // Generate a random double in the range [0,1]
-//                randTest = prg->randD(0,1);
-
-//                // Test if the number is less than or equal to the probablilty
-//                // (The probablity of this being true is the same as the probability itself)
-//                // Infect the next state of the current node if true
-//                if (randTest <= m_prob) {
-//                    currentNode->setAttr(m_infected_next_state, true);
-//                }
-//            }
-//        }
-//    }
-//     For each node, load the next state into the current state
+    // For each node, load the next state into the current state
     size_t i = 0;
     for (Nodes::Pair np : nodes()) {
         NodePtr currentNode = np.node();
 
         // if the node is already infected, there's nothing to do
         if (currentNode->attr(m_infectedAttrId).toBool()) {
+            ++i;
             continue;
         }
 
-        currentNode->setAttr(m_infectedAttrId, Value(nextInfectedStates.at(i)).toBool());
+        bool inf = Value(nextInfectedStates.at(i)).toBool();
+        currentNode->setAttr(m_infectedAttrId, inf);
         ++i;
     }
 
-
-//    for (Nodes::Pair np : nodes()) {
-//        currentNode = np.node();
-//        next_state_infection = currentNode->attrs().value(m_infected+1).toBool();
-//        currentNode->setAttr(m_infected, next_state_infection);
-//    }
     return true;
 }
 } // evoplex
